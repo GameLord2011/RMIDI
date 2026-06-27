531d6c1ae8eb01e5124c2beed445fd7966fb469a
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2026-06-27 14:09:49 -0400

    Just gonna do this here because Smert app control says no for now and I cant listen to music because my brother is a nark so im js gonna stop here before I crash out.

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ struct Chunk {
 struct Message {
     delta: Vec<u32>, // Erm acktually the MIDI format uses Variable Length Quantities to represent the delta.
     status: u8,
-    data: Vec<u8>
+    data: Vec<u8>,
 }
 
 fn main() -> std::io::Result<()> {
@@ -113,56 +113,91 @@ fn main() -> std::io::Result<()> {
     if valid_tracks == false {
         return Err(Error::new(
             ErrorKind::InvalidInput,
-            format!("Chunk {first_inv_chunk} has an invalid flag or no end command!")
-        ))
+            format!("Chunk {first_inv_chunk} has an invalid flag or no end command!"),
+        ));
     }
+
+    // let mut deltas: Vec<u8> = vec![];
 
     for track in tracks {
         let d = track.data;
-        let possible_bytes = [d[0], d[1], d[2], d[3]];
-        let mut bytes: Vec<u8> = vec![];
-        for byte in possible_bytes {
-            if (byte & 0x80) == 0x80 {
-                bytes.insert(bytes.len(), byte);
-                continue;
-            } else {
-                bytes.insert(bytes.len(), byte);
-                break;
+        let mut offset: usize = header.length as usize + 15;
+        // loop {
+            println!("{:X}", offset);
+            let possible_bytes = [d[offset], d[offset + 1], d[offset + 2], d[offset + 3]];
+            let mut bytes: Vec<u8> = vec![];
+            for byte in possible_bytes {
+                if (byte & 0x80) == 0x80 {
+                    bytes.insert(bytes.len(), byte);
+                    continue;
+                } else {
+                    bytes.insert(bytes.len(), byte);
+                    break;
+                }
             }
-        }
 
-        let mut vlq: u32 = 0;
+            let mut vlq: u32 = 0;
 
-        if bytes.len() == 4 {
-            vlq = u32::from_be_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]);
-        } else if bytes.len() == 3 {
-            vlq = u32::from_be_bytes([0, bytes[0], bytes[1], bytes[2]]);
-        } else if bytes.len() == 2 {
-            vlq = u32::from_be_bytes([0, 0, bytes[0], bytes[1]]);
-        } else if bytes.len() == 1 {
-            vlq = u32::from_be_bytes([0, 0, 0, bytes[0]]);
-        }
+            if bytes.len() == 4 {
+                vlq = u32::from_be_bytes([
+                    bytes[0],
+                    bytes[1],
+                    bytes[2],
+                    bytes[3],
+                ]);
+            } else if bytes.len() == 3 {
+                vlq = u32::from_be_bytes([0, bytes[0], bytes[1], bytes[2]]);
+            } else if bytes.len() == 2 {
+                vlq = u32::from_be_bytes([0, 0, bytes[0], bytes[1]]);
+            } else if bytes.len() == 1 {
+                vlq = u32::from_be_bytes([0, 0, 0, bytes[0]]);
+            }
 
-        println!("{}", vlq);
+            let mut n = vlq & 0x7F;
 
-        let mut n = vlq & 0x7F;
+            // More-or-less copy-pasted from ImHex's MIDI 1.0 pattern.
+            if vlq & 0x8000 == 0x8000 {
+                n += ((vlq & 0x7f00) >> 8) * 0x80;
+            }
+            if vlq & 0x800000 == 0x800000 {
+                n += ((vlq & 0x7f0000) >> 8 * 2) * 0x80 * 0x80;
+            }
+            if vlq & 0x80000000 == 0x80000000 {
+                n += ((vlq & 0x7f000000) >> 8 * 3) * 0x80 * 0x80 * 0x80;
+            }
 
-        // More-or-less copy-pasted from ImHex's MIDI 1.0 pattern.
-        if vlq & 0x8000 == 0x8000 {
-            n += ((vlq & 0x7f00) >> 8) * 0x80;
-        }
-        if vlq & 0x800000 == 0x800000 {
-            n += ((vlq & 0x7f0000) >> 8 * 2) * 0x80 * 0x80;
-        }
-        if vlq & 0x80000000 == 0x80000000 {
-            n += ((vlq & 0x7f000000) >> 8 * 3) * 0x80 * 0x80 * 0x80;
-        }
+            println!("Delta 0 is {}", n);
 
-        println!("Delta 0 is {}", n);
+            let status = d[offset + bytes.len()];
 
-        let status = d[bytes.len()];
+            println!("Status 0 is {:X} at offset {:X}", status, offset + bytes.len());
 
-        println!("Status 0 is {} at offset {:X}", status, bytes.len());
+            match status {
+                0x80..=0x8F /* Note off */ => {
+                    offset += bytes.len() + 3;
+                },
+                0x90..=0x9F /* Note on */ => {
+                    offset += bytes.len() + 3;
+                },
+                0xFA /* Start */ => (),
+                0xFB /* Continue */ => (),
+                0xFC /* End */ => (),
+                0xFF /* Meta Event */ => {
+                    println!("Meta chunk.");
+                    match d[offset + bytes.len() + 1] {
+                        0x01..=0x06 => {
+                            offset += d[offset + bytes.len() + 2] as usize;
+                        },  
+                        0x2F => {
+                            break;
+                        },
+                        _ => ()
+                    }
+                },
+                _ => ()
+            }
+            println!("{:X}", offset);
+        // }
     }
 
     Ok(())
